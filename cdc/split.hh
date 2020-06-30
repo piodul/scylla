ce98beb76e814c183b3e68b9c55378f22d3167dc
Piotr Dulikowski <piodul@scylladb.com>
2020-06-30 11:05:50 +0200

    cdc: add interface for producing pre/postimages
    Introduces new methods to the change_processor interface that will cause
    it to produce pre/postimage rows for requested clustering key, or for
    static row.
    
    Introduces logic in split.cc responsible for calling pre/postimage
    methods of the change_processor interface. This does not have any effect
    on generated CDC log mutations yet, because the transformer class has
    empty implementations in place of those methods.

--- cdc/split.hh
+++ cdc/split.hh
@@ -22,6 +22,7 @@
 #pragma once
 
 #include <vector>
+#include <boost/dynamic_bitset.hpp>
 #include "schema_fwd.hh"
 #include "timestamp.hh"
 #include "bytes.hh"
@@ -30,6 +31,12 @@
 class mutation;
 
 namespace cdc {
+
+// Represents a set of column ids of one kind (partition key, clustering key, regular row or static row).
+// There already exists a column_set type, but it keeps ordinal_column_ids, not column_ids (ordinal column ids
+// are unique across whole table, while kind-specific ids are unique only within one column kind).
+// To avoid converting back and forth between ordinal and kind-specific ids, one_kind_column_set is used instead.
+using one_kind_column_set = boost::dynamic_bitset<uint64_t>;
 
 // An object that processes changes from a single, big mutation.
 // It is intended to be used with process_changes_xxx_splitting. Those functions define the order and layout in which
@@ -44,6 +51,16 @@ public:
     //   ts - timestamp of mutation parts
     virtual void begin_timestamp(api::timestamp_type ts) = 0;
 
+    // Tells the processor to produce a preimage for a given clustering/static row.
+    //   ck - clustering key of the row for which to produce a preimage; if nullptr, static row preimage is requested
+    //   columns_to_include - include information about the current state of those columns only, leave others as null
+    virtual void produce_preimage(const clustering_key* ck, const one_kind_column_set& columns_to_include) = 0;
+
+    // Tells the processor to produce a postimage for a given clustering/static row.
+    // Contrary to preimage, this requires data from all columns to be present.
+    //   ck - clustering key of the row for which to produce a postimage; if nullptr, static row postimage is requested
+    virtual void produce_postimage(const clustering_key* ck) = 0;
+
     // Processes a smaller mutation which is a subset of the big mutation.
     // The mutation provided to process_change should be simple enough for it to be possible to convert it
     // into CDC log rows - for example, it cannot represent a write to two columns of the same row, where
@@ -53,7 +70,9 @@ public:
 };
 
 bool should_split(const mutation& base_mutation);
-void process_changes_with_splitting(const mutation& base_mutation, change_processor& processor);
-void process_changes_without_splitting(const mutation& base_mutation, change_processor& processor);
+void process_changes_with_splitting(const mutation& base_mutation, change_processor& processor,
+        bool enable_preimage, bool enable_postimage);
+void process_changes_without_splitting(const mutation& base_mutation, change_processor& processor,
+        bool enable_preimage, bool enable_postimage);
 
 }
