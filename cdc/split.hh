3dd697b1e215b65c7452acc5ea191c6e19d635d9
Piotr Dulikowski <piodul@scylladb.com>
2020-06-30 11:05:50 +0200

    cdc: add interface for producing pre/postimages
    Introduces new methods to the change_processor interface that will cause
    it to produce pre/postimage rows for requested clustering key, or for
    static row.
    
    Introduces logic in split.cc responsible for calling pre/postimage
    methods of the change_processor interface. This does not have any effect
    on generated CDC log mutations yet, because the transformer class has
    empty implementations in place of those methods.

--- cdc/split.hh
+++ cdc/split.hh
@@ -22,6 +22,7 @@
 #pragma once
 
 #include <vector>
+#include <boost/dynamic_bitset.hpp>
 #include "schema_fwd.hh"
 #include "timestamp.hh"
 #include "bytes.hh"
@@ -31,17 +32,25 @@ class mutation;
 
 namespace cdc {
 
+using one_kind_column_set = boost::dynamic_bitset<uint64_t>;
+
 class change_processor {
 public:
     virtual ~change_processor() {};
 
     virtual void begin_timestamp(api::timestamp_type ts) = 0;
 
+    // if ck is null, pre/postimage for static row is requested
+    virtual void produce_preimage(const clustering_key* ck, const one_kind_column_set& columns_to_include) = 0;
+    virtual void produce_postimage(const clustering_key* ck) = 0;
+
     virtual void process_change(const mutation& m) = 0;
 };
 
 bool should_split(const mutation& base_mutation);
-void process_changes_with_splitting(const mutation& base_mutation, change_processor& processor);
-void process_changes_without_splitting(const mutation& base_mutation, change_processor& processor);
+void process_changes_with_splitting(const mutation& base_mutation, change_processor& processor,
+        bool enable_preimage, bool enable_postimage);
+void process_changes_without_splitting(const mutation& base_mutation, change_processor& processor,
+        bool enable_preimage, bool enable_postimage);
 
 }
