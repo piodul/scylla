24482250a811c5ebbd27a757126ffe188b741672
Piotr Dulikowski <piodul@scylladb.com>
2020-07-24 12:46:42 +0200

    config: plug in hints::host_filter object into configuration
    Uses db::hints::host_filter as the type of hinted_handoff_enabled
    configuration option.
    
    Previously, hinted_handoff_enabled used to be a string option, and it
    was parsed later in a separate function during startup. The function
    returned a std::optional<std::unordered_set<sstring>>, whose meaning in
    the context of hints is rather enigmatic for an observer not familiar
    with hints.
    
    Now, hinted_handoff_enabled has type of db::hints::host_filter, and it
    is plugged into the config parsing framework, so there is no need for
    later post-processing.

--- service/storage_proxy.hh
+++ service/storage_proxy.hh
@@ -63,6 +63,7 @@
 #include "service_permit.hh"
 #include "service/client_state.hh"
 #include "cdc/stats.hh"
+#include "db/hints/host_filter.hh"
 
 class reconcilable_result;
 class frozen_mutation_and_schema;
@@ -158,7 +159,7 @@ class storage_proxy : public seastar::async_sharded_service<storage_proxy>, publ
 public:
     using clock_type = lowres_clock;
     struct config {
-        std::optional<std::vector<sstring>> hinted_handoff_enabled = {};
+        db::hints::host_filter hinted_handoff_enabled = {};
         size_t available_memory;
         smp_service_group read_smp_service_group = default_smp_service_group();
         smp_service_group write_smp_service_group = default_smp_service_group();
