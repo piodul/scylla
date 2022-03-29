23a8fd52b6d44fe6bae7648d28c7c1f836fd2995
Piotr Dulikowski <piodul@scylladb.com>
2022-03-29 17:24:27 +0200

    propagate read replica errors through RPC
    Uses the newly introduced exception_variant type to propagate replica
    exceptions through RPC during write operations. The read-related verbs
    are extended so that return an (optional) exception_variant in addition
    to the result.
    
    The conversion of exception to exception_variant occurs only when the
    TYPED_ERRORS_IN_READ_RPC feature is enabled - otherwise exceptions are
    returned as before, using the type-erased seastar RPC exceptions.

--- service/storage_proxy.cc
+++ service/storage_proxy.cc
@@ -3558,8 +3558,11 @@ protected:
             return _proxy->query_mutations_locally(_schema, cmd, _partition_range, timeout, _trace_state);
         } else {
             tracing::trace(_trace_state, "read_mutation_data: sending a message to /{}", ep);
-            return ser::storage_proxy_rpc_verbs::send_read_mutation_data(&_proxy->_messaging, netw::messaging_service::msg_addr{ep, 0}, timeout, *cmd, _partition_range).then([this, ep](rpc::tuple<reconcilable_result, rpc::optional<cache_temperature>> result_and_hit_rate) {
-                auto&& [result, hit_rate] = result_and_hit_rate;
+            return ser::storage_proxy_rpc_verbs::send_read_mutation_data(&_proxy->_messaging, netw::messaging_service::msg_addr{ep, 0}, timeout, *cmd, _partition_range).then([this, ep](rpc::tuple<reconcilable_result, rpc::optional<cache_temperature>, rpc::optional<std::optional<replica::exception_variant>>> result_and_hit_rate) {
+                auto&& [result, hit_rate, opt_exception] = result_and_hit_rate;
+                if (opt_exception.has_value() && opt_exception->has_value()) {
+                    return make_exception_future<rpc::tuple<foreign_ptr<lw_shared_ptr<reconcilable_result>>, cache_temperature>>((**opt_exception).into_exception_ptr());
+                }
                 tracing::trace(_trace_state, "read_mutation_data: got response from /{}", ep);
                 return make_ready_future<rpc::tuple<foreign_ptr<lw_shared_ptr<reconcilable_result>>, cache_temperature>>(rpc::tuple(make_foreign(::make_lw_shared<reconcilable_result>(std::move(result))), hit_rate.value_or(cache_temperature::invalid())));
             });
@@ -3575,8 +3578,11 @@ protected:
             return _proxy->query_result_local(_schema, _cmd, _partition_range, opts, _trace_state, timeout);
         } else {
             tracing::trace(_trace_state, "read_data: sending a message to /{}", ep);
-            return ser::storage_proxy_rpc_verbs::send_read_data(&_proxy->_messaging, netw::messaging_service::msg_addr{ep, 0}, timeout, *_cmd, _partition_range, opts.digest_algo).then([this, ep](rpc::tuple<query::result, rpc::optional<cache_temperature>> result_hit_rate) {
-                auto&& [result, hit_rate] = result_hit_rate;
+            return ser::storage_proxy_rpc_verbs::send_read_data(&_proxy->_messaging, netw::messaging_service::msg_addr{ep, 0}, timeout, *_cmd, _partition_range, opts.digest_algo).then([this, ep](rpc::tuple<query::result, rpc::optional<cache_temperature>, rpc::optional<std::optional<replica::exception_variant>>> result_hit_rate) {
+                auto&& [result, hit_rate, opt_exception] = result_hit_rate;
+                if (opt_exception.has_value() && opt_exception->has_value()) {
+                    return make_exception_future<rpc::tuple<foreign_ptr<lw_shared_ptr<query::result>>, cache_temperature>>((**opt_exception).into_exception_ptr());
+                }
                 tracing::trace(_trace_state, "read_data: got response from /{}", ep);
                 return make_ready_future<rpc::tuple<foreign_ptr<lw_shared_ptr<query::result>>, cache_temperature>>(rpc::tuple(make_foreign(::make_lw_shared<query::result>(std::move(result))), hit_rate.value_or(cache_temperature::invalid())));
             });
@@ -3592,8 +3598,11 @@ protected:
             tracing::trace(_trace_state, "read_digest: sending a message to /{}", ep);
             return ser::storage_proxy_rpc_verbs::send_read_digest(&_proxy->_messaging, netw::messaging_service::msg_addr{ep, 0}, timeout, *_cmd,
                         _partition_range, digest_algorithm(*_proxy)).then([this, ep] (
-                    rpc::tuple<query::result_digest, rpc::optional<api::timestamp_type>, rpc::optional<cache_temperature>> digest_timestamp_hit_rate) {
-                auto&& [d, t, hit_rate] = digest_timestamp_hit_rate;
+                    rpc::tuple<query::result_digest, rpc::optional<api::timestamp_type>, rpc::optional<cache_temperature>, rpc::optional<std::optional<replica::exception_variant>>> digest_timestamp_hit_rate) {
+                auto&& [d, t, hit_rate, opt_exception] = digest_timestamp_hit_rate;
+                if (opt_exception.has_value() && opt_exception->has_value()) {
+                    return make_exception_future<rpc::tuple<query::result_digest, api::timestamp_type, cache_temperature>>((**opt_exception).into_exception_ptr());
+                }
                 tracing::trace(_trace_state, "read_digest: got response from /{}", ep);
                 return make_ready_future<rpc::tuple<query::result_digest, api::timestamp_type, cache_temperature>>(rpc::tuple(d, t ? t.value() : api::missing_timestamp, hit_rate.value_or(cache_temperature::invalid())));
             });
@@ -5104,7 +5113,7 @@ storage_proxy::handle_mutation_failed(const rpc::client_info& cinfo, unsigned sh
         });
 }
 
-future<rpc::tuple<foreign_ptr<lw_shared_ptr<query::result>>, cache_temperature>>
+future<rpc::tuple<foreign_ptr<lw_shared_ptr<query::result>>, cache_temperature, std::optional<replica::exception_variant>>>
 storage_proxy::handle_read_data(const rpc::client_info& cinfo, rpc::opt_time_point t, query::read_command cmd, ::compat::wrapping_partition_range pr, rpc::optional<query::digest_algorithm> oda) {
         tracing::trace_state_ptr trace_state_ptr;
         auto src_addr = netw::messaging_service::get_source(cinfo);
@@ -5133,13 +5142,23 @@ storage_proxy::handle_read_data(const rpc::client_info& cinfo, rpc::opt_time_poi
                 opts.request = da == query::digest_algorithm::none ? query::result_request::only_result : query::result_request::result_and_digest;
                 auto timeout = t ? *t : db::no_timeout;
                 return p->query_result_local(std::move(s), cmd, std::move(pr2.first), opts, trace_state_ptr, timeout);
-            }).finally([&trace_state_ptr, src_ip] () mutable {
+            }).then_wrapped([this, &trace_state_ptr, src_ip] (future<rpc::tuple<foreign_ptr<lw_shared_ptr<query::result>>, cache_temperature>> f) mutable {
+                using final_tuple_type = rpc::tuple<foreign_ptr<lw_shared_ptr<query::result>>, cache_temperature, std::optional<replica::exception_variant>>;
                 tracing::trace(trace_state_ptr, "read_data handling is done, sending a response to /{}", src_ip);
+                if (!f.failed()) {
+                    auto&& [result, temp] = f.get();
+                    return make_ready_future<final_tuple_type>(std::move(result), std::move(temp), std::nullopt);
+                } else if (features().cluster_supports_typed_errors_in_read_rpc()) {
+                    auto ex = replica::encode_replica_exception(f.get_exception());
+                    return make_ready_future<final_tuple_type>(foreign_ptr<lw_shared_ptr<query::result>>(), cache_temperature::invalid(), std::make_optional(std::move(ex)));
+                } else {
+                    return make_exception_future<final_tuple_type>(f.get_exception());
+                }
             });
         });
 }
 
-future<rpc::tuple<foreign_ptr<lw_shared_ptr<reconcilable_result>>, cache_temperature>>
+future<rpc::tuple<foreign_ptr<lw_shared_ptr<reconcilable_result>>, cache_temperature, std::optional<replica::exception_variant>>>
 storage_proxy::handle_read_mutation_data(const rpc::client_info& cinfo, rpc::opt_time_point t, query::read_command cmd, ::compat::wrapping_partition_range pr) {
         tracing::trace_state_ptr trace_state_ptr;
         auto src_addr = netw::messaging_service::get_source(cinfo);
@@ -5166,13 +5185,23 @@ storage_proxy::handle_read_mutation_data(const rpc::client_info& cinfo, rpc::opt
                 unwrapped = ::compat::unwrap(std::move(pr), *s);
                 auto timeout = t ? *t : db::no_timeout;
                 return p->query_mutations_locally(std::move(s), std::move(cmd), unwrapped, timeout, trace_state_ptr);
-            }).finally([&trace_state_ptr, src_ip] () mutable {
+            }).then_wrapped([this, &trace_state_ptr, src_ip] (future<rpc::tuple<foreign_ptr<lw_shared_ptr<reconcilable_result>>, cache_temperature>> f) mutable {
+                using final_tuple_type = rpc::tuple<foreign_ptr<lw_shared_ptr<reconcilable_result>>, cache_temperature, std::optional<replica::exception_variant>>;
                 tracing::trace(trace_state_ptr, "read_mutation_data handling is done, sending a response to /{}", src_ip);
+                if (!f.failed()) {
+                    auto&& [result, temp] = f.get();
+                    return make_ready_future<final_tuple_type>(std::move(result), std::move(temp), std::nullopt);
+                } else if (features().cluster_supports_typed_errors_in_read_rpc()) {
+                    auto ex = replica::encode_replica_exception(f.get_exception());
+                    return make_ready_future<final_tuple_type>(foreign_ptr<lw_shared_ptr<reconcilable_result>>(), cache_temperature::invalid(), std::make_optional(std::move(ex)));
+                } else {
+                    return make_exception_future<final_tuple_type>(f.get_exception());
+                }
             });
         });
 }
 
-future<rpc::tuple<query::result_digest, long, cache_temperature>>
+future<rpc::tuple<query::result_digest, long, cache_temperature, std::optional<replica::exception_variant>>>
 storage_proxy::handle_read_digest(const rpc::client_info& cinfo, rpc::opt_time_point t, query::read_command cmd, ::compat::wrapping_partition_range pr, rpc::optional<query::digest_algorithm> oda) {
         tracing::trace_state_ptr trace_state_ptr;
         auto src_addr = netw::messaging_service::get_source(cinfo);
@@ -5196,8 +5225,18 @@ storage_proxy::handle_read_digest(const rpc::client_info& cinfo, rpc::opt_time_p
                 }
                 auto timeout = t ? *t : db::no_timeout;
                 return p->query_result_local_digest(std::move(s), cmd, std::move(pr2.first), trace_state_ptr, timeout, da);
-            }).finally([&trace_state_ptr, src_ip] () mutable {
+            }).then_wrapped([this, &trace_state_ptr, src_ip] (future<rpc::tuple<query::result_digest, api::timestamp_type, cache_temperature>> f) mutable {
+                using final_tuple_type = rpc::tuple<query::result_digest, api::timestamp_type, cache_temperature, std::optional<replica::exception_variant>>;
                 tracing::trace(trace_state_ptr, "read_digest handling is done, sending a response to /{}", src_ip);
+                if (!f.failed()) {
+                    auto&& [result, timestamp, temp] = f.get();
+                    return make_ready_future<final_tuple_type>(std::move(result), timestamp, std::move(temp), std::nullopt);
+                } else if (features().cluster_supports_typed_errors_in_read_rpc()) {
+                    auto ex = replica::encode_replica_exception(f.get_exception());
+                    return make_ready_future<final_tuple_type>(query::result_digest(), api::missing_timestamp, cache_temperature::invalid(), std::make_optional(std::move(ex)));
+                } else {
+                    return make_exception_future<final_tuple_type>(f.get_exception());
+                }
             });
         });
 }
