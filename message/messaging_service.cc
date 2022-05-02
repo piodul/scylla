8408c6a0d31ed1a222402cbc1328333182b8ac42
Piotr Dulikowski <piodul@scylladb.com>
2022-05-02 12:51:37 +0200

    storage_proxy: add per partition rate limit info to write RPC
    Adds db::per_partition_rate_limit::info parameter to the write RPC. The
    rate limit info controls the behavior of the rate limiter on the
    replica.

--- message/messaging_service.cc
+++ message/messaging_service.cc
@@ -34,6 +34,7 @@
 #include "service/paxos/proposal.hh"
 #include "replica/exceptions.hh"
 #include "serializer.hh"
+#include "db/per_partition_rate_limit_info.hh"
 #include "idl/consistency_level.dist.hh"
 #include "idl/tracing.dist.hh"
 #include "idl/result.dist.hh"
@@ -59,6 +60,7 @@
 #include "idl/raft.dist.hh"
 #include "idl/group0.dist.hh"
 #include "idl/replica_exception.dist.hh"
+#include "idl/per_partition_rate_limit_info.dist.hh"
 #include "idl/storage_proxy.dist.hh"
 #include "serializer_impl.hh"
 #include "serialization_visitors.hh"
@@ -87,6 +89,7 @@
 #include "idl/group0.dist.impl.hh"
 #include "idl/view.dist.impl.hh"
 #include "idl/replica_exception.dist.impl.hh"
+#include "idl/per_partition_rate_limit_info.dist.impl.hh"
 #include "idl/storage_proxy.dist.impl.hh"
 #include <seastar/rpc/lz4_compressor.hh>
 #include <seastar/rpc/lz4_fragmented_compressor.hh>
