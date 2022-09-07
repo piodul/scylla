eb56d1e35ceeaf9673921c1cff389218b1e073b2
Wojciech Mitros <wojciech.mitros@scylladb.com>
2022-09-07 13:47:46 +0200

    test: check that wasmtime functions yield
    The new implementation for WASM UDFs allows executing the UDFs
    in pieces. This commit adds a test asserting that the UDF is in fact
    divided and that each of the execution segments takes no longer than
    1ms.

---
+++ test/boost/wasm_test.cc
@@ -0,0 +1,84 @@
+#include "lang/wasm.hh"
+#include "lang/wasm_instance_cache.hh"
+#include "rust/wasmtime_bindings.hh"
+#include "seastar/coroutine/maybe_yield.hh"
+#include <chrono>
+#include <seastar/core/lowres_clock.hh>
+#include <seastar/testing/test_case.hh>
+#include <seastar/core/coroutine.hh>
+
+SEASTAR_TEST_CASE(test_long_udf_yields) {
+    auto wasm_engine = wasmtime::create_engine();
+    auto wasm_cache = std::make_unique<wasm::instance_cache>(100 * 1024 * 1024, std::chrono::seconds(1));
+    auto wasm_ctx = wasm::context(*wasm_engine, "fib", wasm_cache.get());
+    // Recursive fibonacci function
+    wasm::compile(wasm_ctx, {}, R"(
+(module
+  (type (;0;) (func (param i64) (result i64)))
+  (func (;0;) (type 0) (param i64) (result i64)
+    (local i64 i32)
+    local.get 0
+    i64.const 2
+    i64.lt_s
+    if  ;; label = @1
+      local.get 0
+      return
+    end
+    loop  ;; label = @1
+      local.get 0
+      i64.const 1
+      i64.sub
+      call 0
+      local.get 1
+      i64.add
+      local.set 1
+      local.get 0
+      i64.const 3
+      i64.gt_s
+      local.get 0
+      i64.const 2
+      i64.sub
+      local.set 0
+      br_if 0 (;@1;)
+    end
+    local.get 0
+    local.get 1
+    i64.add)
+  (func (;1;) (type 0) (param i64) (result i64)
+    local.get 0
+    call 0)
+  (memory (;0;) 2)
+  (global (;0;) i32 (i32.const 1024))
+  (export "memory" (memory 0))
+  (export "fib" (func 1))
+  (export "_scylla_abi" (global 0))
+  (data (;0;) (i32.const 1024) "01"))
+)");
+
+    auto argv = wasmtime::get_val_vec();
+    argv->push_i64(42);
+    auto rets = wasmtime::get_val_vec();
+    rets->push_i32(0);
+
+    auto store = wasmtime::create_store(wasm_ctx.engine_ptr);
+    auto instance = wasmtime::create_instance(wasm_ctx.engine_ptr, **wasm_ctx.module, *store);
+    auto func = wasmtime::create_func(*instance, *store, wasm_ctx.function_name);
+    auto memory = wasmtime::get_memory(*instance, *store);
+
+    auto fut = wasmtime::get_func_future(*store, *func, *argv, *rets);
+    bool stop = false;
+
+    using namespace std::chrono;
+    auto max_duration = duration(nanoseconds(0));
+    auto steps = 0;
+    while (!stop) {
+        auto start = system_clock::now();
+        stop = fut->resume();
+        max_duration = std::max(max_duration, system_clock::now() - start);
+        ++steps;
+    }
+    BOOST_REQUIRE(max_duration < duration(milliseconds(100)));
+    BOOST_REQUIRE(steps > 1);
+    BOOST_CHECK_EQUAL(rets->pop_val()->i64(), 267914296);
+    co_return;
+}
