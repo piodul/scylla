70c74e0e94c63197f39b77af565a6a2be16f2366
Piotr Dulikowski <piodul@scylladb.com>
2024-01-26 18:23:43 +0100

    storage_service: gossip tokens and cdc generation in raft topology mode
    A mixed raft/legacy cluster can happen when entering recovery mode, i.e.
    when the group 0 upgrade state is set to 0 and a rolling restart is
    performed. Legacy nodes expect at least information about tokens,
    otherwise an internal error occurs in the handle_state_normal function.
    Therefore, make nodes that use raft topology behave well with respect to
    other nodes.

--- service/storage_service.cc
+++ service/storage_service.cc
@@ -398,6 +398,7 @@ future<> storage_service::sync_raft_topology_nodes(mutable_token_metadata_ptr tm
         // Also ip -> id mapping is needed for address map recreation on reboot
         if (is_me(host_id)) {
             co_await _sys_ks.local().update_tokens(rs.ring.value().tokens);
+            co_await set_gossip_tokens(_gossiper, rs.ring.value().tokens, _topology_state_machine._topology.current_cdc_generation_id);
             co_await _gossiper.add_local_application_state({{ gms::application_state::STATUS, gms::versioned_value::normal(rs.ring.value().tokens) }});
         } else if (ip && !is_me(*ip)) {
             // In replace-with-same-ip scenario the replaced node IP will be the same
